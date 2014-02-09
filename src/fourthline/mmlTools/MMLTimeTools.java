38608d538f2d4fa55ba7c2e7029f8825003e2e7c
fourthline <fourthline@live.jp>
2014-02-09 22:33:12 +0900

    mmltools ( 2011-2013 )

---
+++ src/fourthline/mmlTools/MMLTimeTools.java
@@ -0,0 +1,92 @@
+package fourthline.mmlTools;
+
+
+import java.util.Map;
+
+import fourthline.mmlTools.core.MMLTools;
+
+
+/**
+ * マビノギでの演奏時間
+ * @author たんらる
+ *
+ */
+public class MMLTimeTools extends MMLTools {
+	public final static int CHECK_RANGE_N = 5;
+	
+	/**
+	 * 音域チェックテーブル
+	 * リュート
+	 * O1e～O7e  　(16 @ 88)
+	 * フルート
+	 * O4c～O6b  (48 @ 83)
+	 * ホイッスル
+	 * O5c～O7e  (60 @ 88)
+	 * シャリュモー
+	 * O2c～O4b  (24 @ 59)
+	 * チューバ
+	 * O1c～O4b  (12 @ 59)
+	 */
+	final private int minTable[] = {
+			16, 48, 60, 24, 12
+	};
+	final private int maxTable[] = {
+			88, 83, 88, 59, 59
+	};
+
+	public MMLTimeTools(String mml, boolean drumMode) throws UndefinedTickException {
+		super(mml);
+		
+		parseMMLforMabinogi();
+		parsePlayMode(drumMode);
+	}
+
+	
+	public boolean[] checkRange() {
+		fourthline.mmlTools.core.MelodyParser parser[] = {
+				melodyParser, chord1Parser, chord2Parser
+		};
+		
+		boolean result[] = new boolean[minTable.length];
+		for (int i = 0; i < result.length; i++)
+			result[i] = true;
+		
+		// 楽器ごとの音域チェック
+		for (int tableIndex = 0; tableIndex < minTable.length; tableIndex++) {
+			// パートごとの音域チェック
+			for (int i = 0; i < parser.length; i++) {
+				int min = parser[i].getMinNote();
+				int max = parser[i].getMaxNote();
+				if ( (min < 0) || (max < 0) )
+					continue;
+				
+				if ( (minTable[tableIndex] > min) ||
+						(maxTable[tableIndex] < max) ) {
+					result[tableIndex] = false;
+					break;
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	
+	public Map<Integer, Integer> getPlayList() throws UndefinedTickException {
+		return playParser.getTempoList();
+	}
+	
+	public Map<Integer, Integer> getMelodyList() throws UndefinedTickException {
+		return melodyParser.getTempoList();
+	}
+	
+	public Map<Integer, Integer> getChord1List() throws UndefinedTickException {
+		return chord1Parser.getTempoList();
+	}
+	
+	public Map<Integer, Integer> getChord2List() throws UndefinedTickException {
+		return chord2Parser.getTempoList();
+	}
+	
+
+}
---
+++ src/fourthline/mmlTools/XylophoneMML.java
@@ -0,0 +1,124 @@
+package fourthline.mmlTools;
+
+import fourthline.mmlTools.core.MMLTokenizer;
+
+
+/**
+ * 通常のMMLからシロフォン用のMMLに変換する
+ * @author たんらる
+ *
+ */
+public class XylophoneMML {
+	/**
+	 * falseのとき、通常→シロフォン変換
+	 * trueのとき、シロフォン→通常変換
+	 */
+	private boolean backMode = false;
+	
+
+	/**
+	 * @return 変換後のMML
+	 */
+	public String conv(String mml) throws UndefinedTickException {
+		return this.conv(mml, false);
+	}
+	
+	/**
+	 * @return 変換後のMML
+	 */
+	public String conv(String mml, boolean backMode) throws UndefinedTickException {
+		MMLTokenizer mt = new MMLTokenizer(mml);
+		this.backMode = backMode;
+		
+		StringBuilder sb = new StringBuilder();
+		
+		boolean toHighOct = false; // a, a+, b-, b, c- の領域時にtrue
+		
+		while (mt.hasNext()) {
+			String token = mt.next();
+			
+			System.out.print(" ["+token+"] ");
+			
+			boolean nextTo = false;
+			if ( MMLTokenizer.isNote(token.charAt(0)) ) {
+				System.out.print("*");
+				if (token.charAt(0) == 'n' || token.charAt(0) == 'N') {
+					token = absConv(token);
+				} else {
+					nextTo = isHighPos(token);
+
+					if (!backMode) {
+						if ( (toHighOct) && (!nextTo) ) {
+							sb.append("<");
+						}
+						if ( (!toHighOct) && (nextTo) ) {
+							sb.append(">");
+						}
+					} else {
+						if ( (toHighOct) && (!nextTo) ) {
+							sb.append(">");
+						}
+						if ( (!toHighOct) && (nextTo) ) {
+							sb.append("<");
+						}
+					}
+
+					toHighOct = nextTo;
+				}
+			}
+						
+			sb.append(token);
+		}
+
+		String result = sb.toString();
+		result = result.replaceAll("><", "");
+		result = result.replaceAll("<>", "");
+		
+		return result;
+	}
+
+	
+
+	/**
+	 * n, N 絶対値の変換
+	 * @param token
+	 * @return
+	 */
+	private String absConv(String token) {
+		int note = Integer.parseInt( token.substring(1) );
+		int tNote = note%12;
+		
+		switch (tNote) {
+		case 9: // A
+		case 10: // A#
+		case 11: // B
+			if (!backMode) {
+				note += 12;
+			} else {
+				note -= 12;
+			}
+		}
+		
+		return ""+token.charAt(0)+note;
+	}
+	
+	private boolean isHighPos(String token) {
+		boolean result = false;
+		token = MMLTokenizer.noteName(token);
+		token = token.toLowerCase();
+		
+		if ( (token.equals("a") ) ||
+				(token.equals("a+") ) ||
+				(token.equals("a#") ) ||
+				(token.equals("b-") ) ||
+				(token.equals("b") ) ||
+				(token.equals("c-") ) ) {
+			System.out.print("!");
+			result = true;
+		}
+		
+		return result;
+	}
+	
+	
+}
