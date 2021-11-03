e06828828acca8caee529253b0989253459e519f
たんらる <1832571+fourthline@users.noreply.github.com>
2021-11-03 15:54:41 +0900

    Splash分離

--- src/fourthline/mabiicco/MabiIcco.java
+++ src/fourthline/mabiicco/MabiIcco.java
@@ -5,9 +5,6 @@
 package fourthline.mabiicco;
 
 import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Dimension;
-import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
@@ -15,17 +12,10 @@ import java.util.List;
 
 import javax.sound.midi.InvalidMidiDataException;
 import javax.swing.Action;
-import javax.swing.ImageIcon;
-import javax.swing.JDialog;
 import javax.swing.JFileChooser;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JProgressBar;
-import javax.swing.JTextArea;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.border.LineBorder;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
@@ -48,62 +38,15 @@ import fourthline.mmlTools.MMLTrack;
  */
 public final class MabiIcco {
 	private final String args[];
-	private final JDialog splash = new JDialog();
-	private final SplashPanel splashPanel = new SplashPanel();
-
-	private final class SplashPanel extends JPanel {
-		private static final long serialVersionUID = 2210455372955295858L;
-		private static final int WIDTH = 300;
-		private static final int HEIGHT = 220; 
-		private final ImageIcon img;
-		private final JProgressBar progress = new JProgressBar();
-		private final JLabel version = new JLabel();
-		private final JTextArea textArea = new JTextArea();
-		private SplashPanel() {
-			super();
-			setLayout(null);
-			img = AppResource.getImageIcon("/img/title.png");
-			setPreferredSize(new Dimension(WIDTH, HEIGHT));
-			progress.setMaximum(100);
-			add(progress);
-			progress.setBounds(4, 206, 292, 12);
-			add(version);
-			version.setText("Version: "+AppResource.getVersionText());
-			version.setBounds(160, 60, 120, 14);
-			add(textArea);
-			textArea.setEditable(false);
-			textArea.setBounds(20, 100, 180, 100);
-			setOpaque(false);
-			setBorder(new LineBorder(Color.GRAY, 1, false));
-		}
-
-		@Override
-		public void paint(Graphics g) {
-			g.drawImage(img.getImage(), 0, 0, this);
-			super.paint(g);
-		}
-
-		private void updateProgress(String s, int v) {
-			textArea.setText(textArea.getText()+s);
-			progress.setValue(v);
-		}
-	}
+	private final Splash splash = new Splash();
 
 	public MabiIcco(String args[]) {
 		this.args = args;
-		splash.getContentPane().add(splashPanel);
-		splash.setUndecorated(true);
-		splash.pack();
-		splash.setLocationRelativeTo(null);
 		splash.setVisible(true);
 	}
 
-	private void updateProgress(String str, int progress) {
-		splashPanel.updateProgress(str, progress);
-	}
-
 	public void start() {
-		updateProgress(AppResource.appText("init.midi"), 10);
+		splash.updateProgress(AppResource.appText("init.midi"), 10);
 		initialize();
 	}
 
@@ -111,7 +54,7 @@ public final class MabiIcco {
 		try {
 			// initialize
 			MabiDLS.getInstance().initializeMIDI();
-			updateProgress("OK\n", 20);
+			splash.updateProgress("OK\n", 20);
 
 			MMLTrack.setTempoAllowChardPartFunction(t -> {
 				InstType type = MabiDLS.getInstance().getInstByProgram(t).getType();
@@ -119,18 +62,18 @@ public final class MabiIcco {
 			});
 
 			// loading DLS
-			updateProgress(AppResource.appText("init.dls"), 20);
+			splash.updateProgress(AppResource.appText("init.dls"), 20);
 			if ( !loadDLSFiles(20, 70) ) {
 				JOptionPane.showMessageDialog(null, AppResource.appText("error.needDls"), "ERROR", JOptionPane.ERROR_MESSAGE);
 				System.exit(1);
 			}
-			updateProgress("OK\n", 90);
+			splash.updateProgress("OK\n", 90);
 
 			// create MainFrame
 			ActionDispatcher dispatcher = ActionDispatcher.getInstance();
 			MainFrame mainFrame = new MainFrame(dispatcher);
 			mainFrame.setTransferHandler(new FileTransferHandler(dispatcher));
-			updateProgress("", 100);
+			splash.updateProgress("", 100);
 			dispatcher.setMainFrame(mainFrame).initialize();
 			if (dispatcher.recoveryCheck()) {
 			} else if (args.length > 0) {
@@ -189,7 +132,7 @@ public final class MabiIcco {
 		for (File file : dlsFiles) {
 			dls.loadingDLSFile(file);
 			progress += progressStep;
-			updateProgress("", (int)progress);
+			splash.updateProgress("", (int)progress);
 		}
 
 		if (dls.getAvailableInstByInstType(InstType.MAIN_INST_LIST).length > 0) {
---
+++ src/fourthline/mabiicco/Splash.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2021 たんらる
+ */
+
+package fourthline.mabiicco;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+
+import javax.swing.ImageIcon;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JTextArea;
+import javax.swing.border.AbstractBorder;
+import javax.swing.border.LineBorder;
+
+
+public final class Splash extends JDialog {
+	private static final long serialVersionUID = -4276613664298174063L;
+	private final SplashPanel splashPanel = new SplashPanel();
+	public Splash() {
+		getContentPane().add(splashPanel);
+		setUndecorated(true);
+		pack();
+		setLocationRelativeTo(null);
+	}
+
+	public void updateProgress(String s, int v) {
+		splashPanel.textArea.setText(splashPanel.textArea.getText()+s);
+		splashPanel.progress.setValue(v);
+	}
+
+	private final class SplashPanel extends JPanel {
+		private static final long serialVersionUID = 2210455372955295858L;
+		private static final int WIDTH = 300;
+		private static final int HEIGHT = 220; 
+		private final ImageIcon img;
+		private final JProgressBar progress = new JProgressBar();
+		private final JLabel version = new JLabel();
+		private final JTextArea textArea = new JTextArea();
+		private SplashPanel() {
+			super();
+			setLayout(null);
+			img = AppResource.getImageIcon("/img/title.png");
+			setPreferredSize(new Dimension(WIDTH, HEIGHT));
+			progress.setMaximum(100);
+			add(progress);
+			progress.setBounds(4, 206, 292, 12);
+			add(version);
+			version.setText("Version: "+AppResource.getVersionText());
+			version.setBounds(160, 60, 120, 14);
+			add(textArea);
+			textArea.setEditable(false);
+			textArea.setBounds(20, 100, WIDTH-40, 80);
+			textArea.setBorder(new RoundBorder(30, 10));
+			setOpaque(false);
+			setBorder(new LineBorder(Color.GRAY, 1, false));
+		}
+
+		@Override
+		public void paint(Graphics g) {
+			g.drawImage(img.getImage(), 0, 0, this);
+			super.paint(g);
+		}
+
+		private final class RoundBorder extends AbstractBorder {
+			private static final long serialVersionUID = 429150943820077975L;
+			private final int r;
+			private final Insets insets;
+			private RoundBorder(int r, int m) {
+				this.r = r;
+				this.insets = new Insets(m, m, m, m);
+			}
+
+			@Override
+			public void paintBorder(Component c, Graphics g, int x, int y, int w, int h) {
+				Graphics2D g2 = (Graphics2D) g.create();
+				g2.setColor(Color.GRAY);
+				g2.drawRoundRect(0, 0, w-1, h-1, r, r);
+				g2.dispose();
+			}
+
+			@Override
+			public Insets getBorderInsets(Component c) {
+				return insets;
+			}
+		}
+	}
+}
