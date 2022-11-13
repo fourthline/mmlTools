de84303f59e158251f00403b35b79bbff1f5053e
たんらる <1832571+fourthline@users.noreply.github.com>
2022-11-13 13:47:48 +0900

    Splash修正

--- src/jp/fourthline/mabiicco/Splash.java
+++ src/jp/fourthline/mabiicco/Splash.java
@@ -10,6 +10,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Insets;
+import java.awt.RenderingHints;
 
 import javax.swing.ImageIcon;
 import javax.swing.JDialog;
@@ -88,10 +89,9 @@ public final class Splash extends JDialog implements ISplash {
 		@Override
 		public void paintBorder(Component c, Graphics g, int x, int y, int w, int h) {
 			Graphics2D g2 = (Graphics2D) g.create();
-			g2.setColor(new Color(0.7f, 0.7f, 0.7f));
-			g2.drawRoundRect(0, 0, w - 1, h - 1, r, r);
+			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 			g2.setColor(new Color(0.6f, 0.6f, 0.6f));
-			g2.drawRoundRect(1, 1, w - 3, h - 3, r, r);
+			g2.drawRoundRect(0, 0, w - 1, h - 1, r, r);
 			g2.dispose();
 		}
 
