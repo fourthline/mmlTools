39c5860ffc29a6d8d56b51cedcfcc0c27492e240
たんらる <1832571+fourthline@users.noreply.github.com>
2023-10-24 15:52:57 +0900

    TrackListTableTest: add test

--- test/jp/fourthline/mabiicco/ui/mml/TrackListTableTest.java
+++ test/jp/fourthline/mabiicco/ui/mml/TrackListTableTest.java
@@ -5,11 +5,14 @@ package jp.fourthline.mabiicco.ui.mml;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import jp.fourthline.UseLoadingDLS;
 import jp.fourthline.mmlTools.MMLScore;
 import jp.fourthline.mmlTools.MMLTrack;
+import jp.fourthline.mmlTools.core.MMLText;
 
 public class TrackListTableTest extends UseLoadingDLS {
 
@@ -28,4 +31,75 @@ public class TrackListTableTest extends UseLoadingDLS {
 		assertEquals(expect, o.getTableListInfo());
 	}
 
+	@Test
+	public void test_ui1() {
+		MMLScore score = new MMLScore();
+		var track = new MMLTrack().setMML("MML@aaa,bbb,ccccc;");
+		track.setTrackName("track1");
+		score.addTrack(track);
+		var o = new TrackListTable(score.getTrackList());
+		assertEquals("1", o.getValueAt(0, 0));
+		assertEquals("track1", o.getValueAt(0, 1));
+		assertEquals("0: リュート", o.getValueAt(0, 2));
+		assertEquals("*Rank F ( 3, 3, 5 )", o.getValueAt(0, 3));
+
+		assertEquals(String.class, o.getColumnClass(0));
+		assertEquals(String.class, o.getColumnClass(1));
+		assertEquals(String.class, o.getColumnClass(2));
+		assertEquals(String.class, o.getColumnClass(3));
+
+		assertEquals(false, o.isCellEditable(0, 0));
+		assertEquals(false, o.isCellEditable(0, 1));
+		assertEquals(false, o.isCellEditable(0, 2));
+		assertEquals(false, o.isCellEditable(0, 3));
+	}
+
+	@Test
+	public void test_ui2() {
+		MMLScore score = new MMLScore();
+		var track = new MMLTrack().setMML("MML@aaa,bbb,ccccc;");
+		track.setTrackName("track1");
+		score.addTrack(track);
+		var o = new TrackListTable(score.getTrackList(), true);
+		assertEquals(false, o.getValueAt(0, 0));
+		assertEquals("1", o.getValueAt(0, 1));
+		assertEquals("track1", o.getValueAt(0, 2));
+		assertEquals("0: リュート", o.getValueAt(0, 3));
+		assertEquals("*Rank F ( 3, 3, 5 )", o.getValueAt(0, 4));
+
+		assertEquals(Boolean.class, o.getColumnClass(0));
+		assertEquals(String.class, o.getColumnClass(1));
+		assertEquals(String.class, o.getColumnClass(2));
+		assertEquals(String.class, o.getColumnClass(3));
+		assertEquals(String.class, o.getColumnClass(4));
+
+		assertEquals(true, o.isCellEditable(0, 0));
+		assertEquals(false, o.isCellEditable(0, 1));
+		assertEquals(false, o.isCellEditable(0, 2));
+		assertEquals(false, o.isCellEditable(0, 3));
+		assertEquals(false, o.isCellEditable(0, 4));
+	}
+
+	@Test
+	public void test_ui3() {
+		List<MMLText> list = List.of(new MMLText().setMMLText("MML@aaa;"), new MMLText().setMMLText("MML@bbb;"));
+		var track = new MMLTrack().setMML("MML@aaa,bbb,ccccc;");
+		track.setTrackName("track1");
+
+		var o = new TrackListTable(track, list);
+		assertEquals("1", o.getValueAt(0, 0));
+		assertEquals("track1", o.getValueAt(0, 1));
+		assertEquals("0: リュート", o.getValueAt(0, 2));
+		assertEquals("Rank 練習 ( 3, 0, 0 )", o.getValueAt(0, 3));
+
+		assertEquals(String.class, o.getColumnClass(0));
+		assertEquals(String.class, o.getColumnClass(1));
+		assertEquals(String.class, o.getColumnClass(2));
+		assertEquals(String.class, o.getColumnClass(3));
+
+		assertEquals(false, o.isCellEditable(0, 0));
+		assertEquals(false, o.isCellEditable(0, 1));
+		assertEquals(false, o.isCellEditable(0, 2));
+		assertEquals(false, o.isCellEditable(0, 3));
+	}
 }
